8a9fd8fb5f8bc5e8b9128c21145d3c4fa835bbfd
hwajingh <51863703+hwajingh@users.noreply.github.com>
2020-07-28 17:17:56 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,129 @@
+import React, {useState} from 'react';
+import {View, StyleSheet, FlatList, Alert} from 'react-native';
+import { v4 as uuidv4 } from 'uuid';
+
+import Header from './components/Header';
+import ListItem from './components/ListItem';
+import AddItem from './components/AddItem';
+
+const App = () => {
+  const [items, setItems] = useState([
+    {
+      id: uuidv4(),
+      text: 'Milk',
+    },
+    {
+      id: uuidv4(),
+      text: 'Eggs',
+    },
+    {
+      id: uuidv4(),
+      text: 'Bread',
+    },
+    {
+      id: uuidv4(),
+      text: 'Juice',
+    },
+  ]);
+
+  const [editStatus, editStatusChange] = useState(false);
+
+  const [editItemDetail, editItemDetailChange] = useState({
+    id: null,
+    text: null,
+  });
+
+  const [checkedItems, checkedItemChange] = useState([]);
+
+  const deleteItem = id => {
+    setItems(prevItems => {
+      return prevItems.filter(item => item.id !== id);
+    });
+  };
+
+  const saveEditItem = (id, text) => {
+    setItems(prevItems => {
+      return prevItems.map(item =>
+        item.id === editItemDetail.id ? {id, text: editItemDetail.text} : item,
+      );
+    });
+
+    editStatusChange(!editStatus);
+  };
+
+  const handleEditChange = text => {
+    editItemDetailChange({id: editItemDetail.id, text});
+  };
+
+  const addItem = text => {
+    if (!text) {
+      Alert.alert(
+        'No item entered',
+        'Please enter an item when adding to your shopping list',
+        [
+          {
+            text: 'Understood',
+            style: 'cancel',
+          },
+        ],
+        {cancelable: true},
+      );
+    } else {
+      setItems(prevItems => {
+        return [{id: uuidv4(), text}, ...prevItems];
+      });
+    }
+  };
+
+  const editItem = (id, text) => {
+    editItemDetailChange({
+      id,
+      text,
+    });
+    return editStatusChange(!editStatus);
+  };
+
+  const itemChecked = (id, text) => {
+    const isChecked = checkedItems.filter(checkedItem => checkedItem.id === id);
+    isChecked.length
+      ? // remove item from checked items state (uncheck)
+        checkedItemChange(prevItems => {
+          return [...prevItems.filter(item => item.id !== id)];
+        })
+      : // Add item to checked items state
+        checkedItemChange(prevItems => {
+          return [...prevItems.filter(item => item.id !== id), {id, text}];
+        });
+  };
+
+  return (
+    <View style={styles.container}>
+      <Header title="Shopping List" />
+      <AddItem addItem={addItem} />
+      <FlatList
+        data={items}
+        renderItem={({item}) => (
+          <ListItem
+            item={item}
+            deleteItem={deleteItem}
+            editItem={editItem}
+            isEditing={editStatus}
+            editItemDetail={editItemDetail}
+            saveEditItem={saveEditItem}
+            handleEditChange={handleEditChange}
+            itemChecked={itemChecked}
+            checkedItems={checkedItems}
+          />
+        )}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
